a355b2962077ddd8cbff3e1c7a19b490a5dbe068
JHardisty333 <jessica1990_37@hotmail.com>
2021-08-29 16:12:17 -0600

    added Script

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// var to display todays date at the top of the screen
+var todayDate = moment().format("dddd, MMM Do YYYY, h:mm.ss.");
+
+// saveBtn function
+$(document).ready(function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    // saving the enter the information to local storage 
+    localStorage.setItem(time, text);
+})
+
+// track time 
+function trackTime() {
+    var timeNow = moment().hour();
+
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+    // information for local storage
+    $("#12pm .description").val(localStorage.getItem("#12pm"));
+    $("#1pm .description").val(localStorage.getItem("#1pm"));
+    $("#2pm .description").val(localStorage.getItem("#2pm"));
+    $("#3pm .description").val(localStorage.getItem("#3pm"));
+    $("#4pm .description").val(localStorage.getItem("#4pm"));
+    $("#5pm .description").val(localStorage.getItem("#5pm"));
+    $("#6pm .description").val(localStorage.getItem("#6pm"));
+    $("#7pm .description").val(localStorage.getItem("#7pm"));
+    $("#8pm .description").val(localStorage.getItem("#8pm"));
+    $("#9pm .description").val(localStorage.getItem("#9pm"));
+    $("#10pm .description").val(localStorage.getItem("#10pm"));
+    $("#11pm .description").val(localStorage.getItem("#11pm"));
+    $("#12am .description").val(localStorage.getItem("#12am"));
+    $("#1am .description").val(localStorage.getItem("#1am"));
+    $("#2am .description").val(localStorage.getItem("#2am"));
+    $("#3am .description").val(localStorage.getItem("#3am"));
+
+trackTime();
+
+// accurate refresh time
+
+setInterval(function() {
+$(".currentTime").html(function() {
+    refresh();
+});
+}, 1000);
+
